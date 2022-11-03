764547ba2887dcceefc50694db010cbda90a8bc7
bruk19 <brukteshome21@gmail.com>
2022-11-03 14:14:33 +0300

    Impelement reducer

---
+++ src/Redux/reducer.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const GET_MESSAGE = "HELLE_RAILS_REACT";
+const InitialState = [];
+
+const fetchData = () => async (dispatch) => {
+  const result = await axios.get("http://127.0.0.1:3005/api/v1/greetings");
+  return dispatch({ type: GET_MESSAGE, payload: result.data.greeting });
+};
+
+const greetReducer = (state = InitialState, action) => {
+  switch (action.type) {
+    case GET_MESSAGE:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export { fetchData };
+export default greetReducer;
